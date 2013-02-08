5a0bb7f1739da701d1853973cac4b1af5013f200
Łukasz Korecki <lukasz@coffeesounds.com>
2013-02-08 00:01:13 +0000

    something like that

--- proxy.rb
+++ proxy.rb
@@ -2,34 +2,36 @@ require 'eventmachine'
 require 'em-proxy'
 
 
-# hard coded for now
+class ProxyFactory # ugh
+  LOCALHOST="127.0.0.1"
+  def initialize bind_to={ host: LOCALHOST, port: 3000} , workers=[ { name: "worker_1", host: LOCALHOST, port: 3001}]
+    @host = bind_to.fetch :host
+    @port = bind_to.fetch :port
+    @wokers = workers
+    @logger = ::Logger.new STDOUT # TODO configurable?
+  end
 
-INCOMING = 3000
+  def start
+    Proxy.start(host: @host, port: @port) do |conn|
+      @workers.each_with_index do |worker|
+        conn.server worker[:name].to_sym, host: worker[:host], port: worker[:port]
+      end
 
-WORKERS = [3001, 3002]
+
+      conn.on_data { |data| @logger.info [:on_data, data] ;  data }
+      conn.on_response { |server, resp| @logger.info [ :on_response, server, resp ] ;  resp }
+    end
+  end
+end
+
+# not sure about :symbols here
+workers = [
+  { name: 'worker_1', port: 3001, host: '127.0.0.1' },
+  { name: 'worker_2', port: 3002, host: '127.0.0.1' }
+]
 
 t1 = Thread.new do
-  Proxy.start(host: "127.0.0.1", port: INCOMING) do |conn|
-    WORKERS.each_with_index do |port, idx|
-      conn.server "worker_#{idx}".to_sym, host: "127.0.0.1", port:  port
-    end
-
-
-    conn.on_data { |data| p [:on_data, data] ;  data }
-    conn.on_response { |server, resp| p [ :on_response, server, resp ] ;  resp }
-  end
+  ProxyFactory.new({host: '127.0.0.1', port: 3000}, workers).start
 
 end
-
-t2 = Thread.new do
-  Proxy.start(host: "127.0.0.1", port: INCOMING-1) do |conn|
-    WORKERS.each_with_index do |port, idx|
-      conn.server "worker_#{idx}".to_sym, host: "127.0.0.1", port:  port
-    end
-
-
-    conn.on_data { |data| p [:on_data, data] ;  data }
-    conn.on_response { |server, resp| p [ :on_response, server, resp ] ;  resp }
-  end
-end
-[t1, t2].map(&:join)
+[t1].map(&:join)
